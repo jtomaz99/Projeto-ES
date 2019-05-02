8d6ecd895aff290c158c170db360599376212066
José Tomáz <jtomaz1999@gmail.com>
2019-05-02 17:57:39 -0300

    Creating application_controller
    Create the first general controller

--- Pethelper/app/controllers/application_controller.rb
+++ Pethelper/app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+end
+
+def current_user
+  if session[:user_id]
+    @current_user = User.find(session[:user_id])
+  end
+end
+helper_method :current_user
+
+def authorize
+  redirect_to login_url unless current_user
+end
 end
